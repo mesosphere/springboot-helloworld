25faaee5ede7c41bc417f73e06ae49fc6acff769
cmays20 <cmays@outlook.com>
2019-09-16 12:39:34 -0500

    Initial load

--- src/main/java/com/d2iq/helloworld/greeting/GreetingController.java
+++ src/main/java/com/d2iq/helloworld/greeting/GreetingController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class GreetingController {
     @Value("${app.defaultName}")
     private String defaultName;
